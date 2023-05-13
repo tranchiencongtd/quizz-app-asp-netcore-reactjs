1100e3af61a54abb1adf53e79d7144b577e28aa5
tranchiencongtd <tranchiencongtd@gmail.com>
2023-05-13 19:20:56 +0700

    add images

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 import Layout from './components/Layout';
+import Authenticate from './components/Authenticate';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route element={<Layout />} >
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/result" element={<Result />} />
+        <Route element={<Authenticate />}> 
+          <Route element={<Layout />} >
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/result" element={<Result />} />
+          </Route>
         </Route>
+        
       </Routes>
     </BrowserRouter>
   );
